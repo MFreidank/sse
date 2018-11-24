925eb9519ddad347b2e67f75d7c880215e6578e5
Pablo Escodebar <escodebar@gmail.com>
2018-11-25 00:07:28 +0100

    Refactor args and opts
    use fixtures to prevent repeating code

--- test/test_management_commands.py
+++ test/test_management_commands.py
@@ -8,22 +8,26 @@ import pytest
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
 
-@pytest.mark.django_db
-def test_load_athena_data_creates_domains_correctly():
-    args = []
-    opts = {
+
+@pytest.fixture
+def args():
+    yield []
+
+
+@pytest.fixture
+def opts():
+    yield {
 		'concept_file': os.path.join(DATA_DIR, 'concepts.csv'),
 		'synonyms_file': os.path.join(DATA_DIR, 'synonyms.csv'),
     }
+
+
+@pytest.mark.django_db
+def test_load_athena_data_creates_domains_correctly(args, opts):
     call_command('load_athena_data', *args, **opts)
     assert 2 == Domain.objects.count()
 
 @pytest.mark.django_db
-def test_load_athena_data_creates_entities_correctly():
-    args = []
-    opts = {
-		'concept_file': os.path.join(DATA_DIR, 'concepts.csv'),
-		'synonyms_file': os.path.join(DATA_DIR, 'synonyms.csv'),
-    }
+def test_load_athena_data_creates_entities_correctly(args, opts):
     call_command('load_athena_data', *args, **opts)
     assert 4 == Entity.objects.count()
