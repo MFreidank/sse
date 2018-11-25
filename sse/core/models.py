beef943909ce270bc9fa6f330b58f8027c96469a
Pablo Escodebar <escodebar@gmail.com>
2018-11-25 04:24:21 +0100

    Refactor load_pubmed_data

--- sse/core/models.py
+++ sse/core/models.py
@@ -10,7 +10,7 @@ class Article(models.Model):
 class Author(models.Model):
     affiliation = models.TextField()
     articles = models.ManyToManyField('Article')
-    email = models.EmailField()
+    email = models.EmailField(null=True)
     name = models.TextField()
 
 
--- sse/management/commands/load_pubmed_data.py
+++ sse/management/commands/load_pubmed_data.py
@@ -1,105 +1,204 @@
-from django.core.management.base import BaseCommand
-import os
-import pickle
-import re
 from Bio import Entrez
 from Bio import Medline
-from sse.core.vocabulary.match_text import generate_matches
+from django.core.management.base import BaseCommand
 from sse.core.entity_engines import extract_entity
 from sse.core.models import Article
 from sse.core.models import Author
 from sse.core.models import Entity
 from sse.core.models import Match
 from sse.core.models import Tag
+from sse.core.vocabulary.match_text import generate_matches
+import os
+import pickle
+import re
 
 class Command(BaseCommand):
 
-    def handle(self, *args, **options):
-        # initialize batches [for Intelligent Backend (TM) ;) ]
-        batch_size = 5
-        pubmed_id_batch = []
-        abstract_batch = []
-        
-        # load text matching automaton
-        automaton_path = os.path.join(os.path.dirname(__file__), "..", "..", 
-            "..", "data", "processed", "vocabulary_automaton.pkl")
-        automaton = pickle.load(open(automaton_path, "rb"))
-        
-        # search for papers and store the PubMed IDs
-        max_documents = 10
-        Entrez.email = "diegovs87@yahoo.fr"
-        handle = Entrez.esearch(db="pubmed", retmax=max_documents, term="liver")
+    article_field_mapping = {
+        "title": "TI",
+        "abstract": "AB",
+        "journal": "JT",
+    }
+    batch_size = 5
+    database = "pubmed"
+    id_list_key = "IdList"
+    max_documents = 10
+    rettype = "medline"
+    search_term = "liver"
+
+    def add_arguments(self, parser):
+        default_api_key = os.environ.get("AZURE_COGNITIVE_API_KEY", None)
+
+        parser.add_argument("--api-key", default=default_api_key, nargs="?", type=str)
+        parser.add_argument("--automaton-file", nargs=1, type=str)
+        parser.add_argument("--entrez-email", nargs=1, type=str)
+
+    def load_automaton(self, **options):
+        with open(options.get("automaton_file"), "rb") as automaton_file:
+            automaton = pickle.load(automaton_file)
+        return automaton
+
+    def configure_entrez(self, **options):
+        Entrez.email = options.get("entrez_email")
+
+    def get_entrez_id_list(self, **options):
+        handle = Entrez.esearch(
+            db=self.database,
+            retmax=self.max_documents,
+            term=self.search_term,
+        )
         results = Entrez.read(handle)
-        ids = results["IdList"]
-        
-        # fetch the papers
-        handle = Entrez.efetch(db="pubmed", retmax=max_documents, id=ids, 
-            rettype="medline")
-        data = []
-        for record in Medline.parse(handle):
-            # Exclude non-english articles
-            if "eng" not in record["LA"]:
-                continue
-        
-            # Article
-            if "TI" not in record.keys():
-                continue
-            title = record["TI"]
-            abstract = None
-            if "AB" in record.keys():
-                abstract = record["AB"]
-            pubmed_id_batch.append(record["PMID"])
-            abstract_batch.append(record["AB"])
-            publication_date = record["MHDA"]
-            journal_title = record["JT"]
-            article = Article.objects.create(title=title, abstract=abstract, 
-                journal=journal_title)
-        
-            if abstract != None:
-                # Add matches from Aho-Corasick Automaton
-                matches = generate_matches(automaton, abstract)
-                for end, (omop_id, match) in matches:
-                    end = end + 1
-                    length = len(match)
-                    offset = end - len(match)
-                    entity = Entity.objects.get(omop_id=omop_id)
-                    match = Match.objects.create(article=article, entity=entity,
-                        offset=offset, length=length)
-        
-            # Author
-            name = None
-            affiliation = None
-            email = None
-            if "AU" in record.keys():
-                name = record["AU"]
-            if "AD" in record.keys():
-                affiliation = record["AD"]
-                email_regex = r"[a-zA-Z0-9._-]+\@[a-zA-Z0-9._-]+\.[a-zA-Z0" + \
-                    "-9_-]+"
-                email_matches = list(re.finditer(email_regex, record["AD"]))
-                if len(email_matches) > 0:
-                    start, end = email_matches[0].span()
-                    email = record["AD"][start:end]
-                author = Author.objects.update_or_create(name=name, 
-                    email=email, affiliation=affiliation)
-                author.articles.add(article)
-        
-            # Tags (MeSH Terms)
-            if "MH" in record.keys():
-                tags = record["MH"]
-                for tag in tags:
-                    description = "TEST"
-                    tag_object = Tag.objects.get_or_create(tag=tag, 
-                        description=description)
-                    tag_object.articles.add(article)
-        
-            # Check if Intelligent Backend batches are ready to be processed
-            if len(pubmed_id_batch) == batch_size:
-                data_batch = { "documents" : [{ "id" : pubmed_id_batch[j], 
-                    "language" : "en", "text" : abstract_batch[j] } for j in 
-                    range(len(pubmed_id_batch)) ] }
-                endpoint = "https://westeurope.api.cognitive.microsoft.com/" + \
-                    "text/analytics/v2.0/entities"
-                #matches = extract_entity(data_batch, api_key=api_key, endpoint=endpoint)
-                pubmed_id_batch = []
-                abstract_batch = []
+        return results[self.id_list_key]
+
+    def get_entrez_records(self, *args, **options):
+        handle = Entrez.efetch(
+            db=self.database,
+            retmax=self.max_documents,
+            id=self.get_entrez_id_list(**options),
+            rettype=self.rettype,
+        )
+        return self.filter_records(Medline.parse(handle))
+
+    def filter_records(self, records):
+        """
+        Filter all records which aren in english and have a title
+        """
+        return list(filter(
+            lambda record: "eng" in record["LA"] and "TI" in record,
+            records
+        ))
+
+    def handle(self, *args, **options):
+        # create entrez data
+        self.records = self.get_entrez_records(*args, **options)
+        self.create_articles(*args, **options)
+        self.create_vocabulary_matches(*args, **options)
+        self.create_authors(*args, **options)
+        self.assign_articles_to_authors(*args, **options)
+        self.create_tags(*args, **options)
+        self.assign_tags_to_articles(*args, **options)
+
+        # create azure data
+        self.cognitive_batches = self.get_cognitive_batches(*args, **options)
+        #self.create_cognitive_matches(*args, **options)
+
+    def get_article_data_as_keywords(self, record):
+        return {
+            model_key: record.get(record_key)
+            for model_key, record_key in self.article_field_mapping.items()
+        }
+
+    def create_articles(self, *args, **options):
+        Article.objects.bulk_create([
+            Article(
+                **self.get_article_data_as_keywords(record)
+            )
+            for record in self.records
+        ])
+
+
+    def create_vocabulary_matches(self, *args, **options):
+        automaton = self.load_automaton(*args, **options)
+        articles = Article.objects.filter(abstract__isnull=False)
+        entities = {
+            entity.omop_id: entity
+            for entity in Entity.objects.all()
+        }
+
+        Match.objects.bulk_create([
+            Match(
+                article=article,
+                entity=entities.get(omop_id),
+                length=len(match),
+                offset=end - len(match) + 1,
+            )
+            for article in articles
+            for end, (omop_id, match) in generate_matches(
+                automaton,
+                article.abstract,
+            )
+            if entities.get(omop_id)
+        ])
+
+    def get_cognitive_batches(self, *args, **options):
+        api_key = options.get('api_key')
+        batch_size = options.get("batch_size", self.batch_size)
+        records = list(filter(lambda record: "AB" in record, self.records))
+
+        batches = [
+            records[index:index + batch_size]
+            for index in range(0, len(records), batch_size)
+        ]
+
+        return [
+            extract_entity({
+                "documents": [
+                    {
+                        "id": record["PMID"],
+                        "language": "en",
+                        "text": record["AB"],
+                    }
+                    for record in batch
+                ]
+            }, api_key=api_key)
+            for batch in batches
+        ]
+
+    def create_cognitive_matches(self, *args, **kwargs):
+        for batch in self.cognitive_batches:
+            for records_index, document in enumerate(batch['documents']):
+                self.create_matches_from_azure_cognitive_document(records_index, document)
+
+    def create_matches_from_azure_cognitive_document(self, records_index, document):
+        found_entities = document.get('entities')
+
+        entity_names = {entity['name'] for entity in found_entities}
+
+        assert False  # continue working here
+
+        Match.objects.bulk_create([
+            Match(
+                article=self.get_article_for_document_id(document_id), # todo
+                entity=entities[found_entity['text']],                 # todo
+                length=match.get('length'),
+                offset=match.get('offset'),
+            )
+            for entity in found_entities
+            for match in entity['matches']
+        ])
+
+    def create_authors(self, *args, **options):
+
+        def get_email(record):
+            email = re.findall(r"[\w\.-]+@[\w\.-]+", record.get("AD"))
+            if email:
+                return email[0].strip('.')
+            return None
+
+        Author.objects.bulk_create([
+            Author(
+                affiliation=record.get("AD"),
+                email=get_email(record),
+                name=name,
+            )
+            for record in self.records
+            for name in record.get("AU", [])
+        ])
+
+    def assign_articles_to_authors(self, *args, **options):
+        pass
+
+    def create_tags(self, *args, **options):
+        tags = {
+            tag
+            for record in self.records
+            for tag in record.get("MH", [])
+        }
+
+        Tag.objects.bulk_create([
+            Tag(tag=tag)
+            for tag in tags
+        ])
+
+    def assign_tags_to_articles(self, *args, **options):
+        pass
